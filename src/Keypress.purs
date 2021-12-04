c9af2d0ccb50fed99a929b8a55a5cd18e210a7b7
Kyosuke Fujimoto <kyoro.f@gmail.com>
2021-12-04 17:59:11 +0900

    Format

--- src/Keypress.purs
+++ src/Keypress.purs
@@ -2,11 +2,9 @@ module Keypress
   ( Key
   , Pressed
   , keypress
-  )
-  where
+  ) where
 
 import Prelude
-
 import Control.Promise (Promise, toAffE)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -14,18 +12,18 @@ import Effect.Aff (Aff)
 
 foreign import keypressImpl :: forall a. (a -> Maybe a) -> Maybe a -> Unit -> Effect (Promise Pressed)
 
-type Pressed = {
-    key :: Maybe Key,
-    ch :: Maybe Char
-}
+type Pressed
+  = { key :: Maybe Key
+    , ch :: Maybe Char
+    }
 
-type Key = {
-    name :: Char,
-    ctrl :: Boolean,
-    meta :: Boolean,
-    shift :: Boolean,
-    sequence :: Char
-}
+type Key
+  = { name :: Char
+    , ctrl :: Boolean
+    , meta :: Boolean
+    , shift :: Boolean
+    , sequence :: Char
+    }
 
 keypress :: Aff Pressed
 keypress = toAffE $ keypressImpl Just Nothing unit
